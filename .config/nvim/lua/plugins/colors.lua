f682ead9cd5221684677071b9ac6cdcb239a76c3
Ben Colegate <BennyC@users.noreply.github.com>
2024-10-19 12:58:50 +0100

    Updated nvim

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -2,11 +2,13 @@ return {
   -- add dracula
   { "Mofiqul/dracula.nvim" },
 
+  { "ellisonleao/gruvbox.nvim" },
+
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "dracula",
     },
   },
 }
